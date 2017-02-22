a85b74a64b97f8a765e6f9e8f8a15c8f1154700e
yoon82 <yoon82@slac.stanford.edu>
2017-02-22 15:56:33 -0800

    remove python scaling

--- pythonScaling/main.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir $1
-bsub -n 2 -o $1/%J.log -q psfehhiprioq mpirun code1.sh $1
--- pythonScaling/test.py
+++
@@ -1,6 +0,0 @@
-#import psana
-import time
-tic = time.time()
-time.sleep(1)
-toc = time.time()
-print toc-tic
